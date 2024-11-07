6e73eb95f089a9654e82871c9f464eb8efa35804
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-11-06 23:30:59 -0300

    :sparkles: feat: admin de clientes + templates html de clientes

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_cnpj', 'client_phone_number', 'client_company')
+    search_fields = ('client_name', 'client_cnpj', 'client_cnpj',)
