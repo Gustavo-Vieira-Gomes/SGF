474f280e227320beb52a7e9e6f3fd818aea93e8f
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-11-06 23:34:06 -0300

    :sparkles: feat: admin companies adicionado

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cnpj', 'company_address', 'phone', 'email', 'updated_at', 'created_at')
+    search_fields = ('name', 'cnpj', )
