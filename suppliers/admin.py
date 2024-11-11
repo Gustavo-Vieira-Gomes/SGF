1408071c98a26462cc60790b0b28880cf746404b
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-11-11 18:17:58 -0300

    :sparkles: feat: admin configurado, templates html prontos

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Supplier
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('supplier_name', 'supplier_cnpj', 'supplier_phone_number', 'supplier_company')
+    search_fields = ('supplier_name', 'supplier_cnpj', 'supplier_cnpj',)
