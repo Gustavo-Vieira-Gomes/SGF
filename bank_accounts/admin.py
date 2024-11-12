52cf96cccd44e8ae5514c11d0ca18862f23ee308
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-11-12 16:57:34 -0300

    :sparkles: feat: templates e admin de contas bancárias criados

--- bank_accounts/admin.py
+++ bank_accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BankAccount
 
-# Register your models here.
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('bank', 'agency_number', 'account_number', 'account_type', 'account_balance', 'account_company')
+    search_fields = ('bank', 'agency_number', 'account_number', 'account_type', 'account_company')
