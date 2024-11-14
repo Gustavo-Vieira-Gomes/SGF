677e7089d2a4dec19c1f8fd8ee7e6f2f3e3d34af
Gustavo-Vieira-Gomes <gvieira.26gomes@gmail.com>
2024-11-13 22:44:33 -0300

    :sparkles: feat: centros de custos adicionados ao admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, CostCenter
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'cnpj', 'company_address', 'phone', 'email', 'updated_at', 'created_at')
     search_fields = ('name', 'cnpj', )
+
+
+@admin.register(CostCenter)
+class CostCenterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
